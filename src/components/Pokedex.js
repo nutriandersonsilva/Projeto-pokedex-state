34df931844b56af7b190ca6e01dfbe5caa777c61
Anderson Silva <nutriandersonsilva@gmail.com>
2022-11-30 17:53:14 -0300

    atividade 4

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { arrayOf } from 'prop-types';
-
 import Pokemon from './Pokemon';
 import { pokemonType } from '../types';
 
@@ -8,28 +7,53 @@ class Pokedex extends React.Component {
   constructor() {
     super();
     this.state = {
-      numeroPokemon: 0,
+      pokemonAtual: 0,
+      nomePokemon: '',
     };
     this.proximoPokemon = this.proximoPokemon.bind(this);
+    this.escolhePokemon = this.escolhePokemon.bind(this);
+    this.definePokemon = this.definePokemon.bind(this);
   }
 
-  proximoPokemon(pokemons) {
+  proximoPokemon(todosPokemons) {
     this.setState((estadoAnterior) => ({
-      numeroPokemon: (estadoAnterior.numeroPokemon + 1) % pokemons,
+      pokemonAtual: (estadoAnterior.pokemonAtual + 1) % todosPokemons,
     }));
   }
 
-  render() {
+  escolhePokemon(nomePokemon) {
+    this.setState({ nomePokemon, pokemonAtual: 0 });
+  }
+
+  definePokemon() {
     const { pokemonList } = this.props;
-    const { numeroPokemon } = this.state;
+    const { nomePokemon } = this.state;
+
+    return pokemonList.filter((pokemon) => {
+      if (nomePokemon === '') return true;
+      return pokemon.type === nomePokemon;
+    });
+  }
+
+  render() {
+    const { pokemonAtual } = this.state;
+    const escolhePokemon = this.definePokemon();
+    const pokemon = escolhePokemon[pokemonAtual];
+
     return (
       <div className="pokedex">
-        <Pokemon pokemon={ pokemonList[numeroPokemon] } />
+        <Pokemon pokemon={ pokemon } />
         <button
           type="button"
-          onClick={ () => this.proximoPokemon(pokemonList.length) }
+          onClick={ () => this.proximoPokemon(escolhePokemon.length) }
         >
           Próximo pokémon
+        </button>
+        <button type="button" onClick={ () => this.escolhePokemon('Fire') }>
+          Fire
+        </button>
+        <button type="button" onClick={ () => this.escolhePokemon('Psychic') }>
+          Psychic
         </button>
       </div>
     );
